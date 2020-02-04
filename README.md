4a70b36398749909e58ca97a5633ce9bde6f00d2
rick Kauffman <rick@rickkauffman.com>
2020-02-04 18:58:14 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# stackstorm-hpe-iolamplifier
-Stackstorm integration pack for HPE iLo Amplifier
+# Ilo Amplifier Integration Pack
+This pack allows you to integrate with iLO Amplifier
+
+## Configuration
+Copy the example configuration in [iloamplifier].yaml.example](./iloamplifier.yaml.example) to
+`/opt/stackstorm/configs/iloamplifier.yaml` and edit as required.
+
+It must contain:
+
+```
+ipaddress - Your iloAmp appliance IP address
+username - iloAmp User name
+password - iloAmp Password
+```
+
+You can also use dynamic values from the datastore. See the
+[docs](https://docs.stackstorm.com/reference/pack_configs.html) for more info.
+
+Example configuration:
+
+```yaml
+---
+  login_host: "10.10.10.10"
+  login_account: "Administrator"
+  login_password: "password"
+```
+You can also run `st2 pack config iloamplifier` and answer the promts
+
+**Note** : When modifying the configuration in `/opt/stackstorm/configs/` please
+           remember to tell StackStorm to load these new values by running
+           `st2ctl reload --register-configs`
+
+
+## Actions
+
+Actions are defined in two groups:
+
+### Individual actions: GET, POST, PUT with under bar will precede each individual action
+* ``get_systems``
+* ``get_chassis``
+* ``get_ethernetinterfaces``
+
+
+### Orquestra Workflows: will not
---
+++ actions/lib/actions.py
@@ -0,0 +1,38 @@
+# (C) Copyright 2019 Hewlett Packard Enterprise Development LP.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# __author__ = "@netwookie"
+# __credits__ = ["Rick Kauffman"]
+# __license__ = "Apache2.0"
+# __maintainer__ = "Rick Kauffman"
+# __email__ = "rick.a.kauffman@hpe.com"
+
+import redfish
+from st2common.runners.base_action import Action
+
+class HpeIloBaseAction(Action):
+    def __init__(self,config):
+        super(HpeIloBaseAction, self).__init__(config)
+        self.client = self._get_client()
+
+    def _get_client(self):
+        login_host = self.config['login_host']
+        login_account = self.config['login_account']
+        login_password = self.config['login_password']
+
+        ## Create a REDFISH object
+        client = redfish.redfish_client(base_url=login_host, username=login_account, \
+                                  password=login_password, default_prefix='/redfish/v1')
+
+        return client
